4eaa4d6f1c0f78985ef9a39fef01da97fa14ff5d
yehor1959 <zaitsev.yehor@gmail.com>
2025-02-28 09:14:25 +0100

    Add Prepend method to DoublyLinkedList

--- src/main/java/org/example/DoublyLinkedList.java
+++ src/main/java/org/example/DoublyLinkedList.java
@@ -70,4 +70,17 @@ public class DoublyLinkedList {
         length--;
         return temp;
     }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            head.prev = newNode;
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
 }

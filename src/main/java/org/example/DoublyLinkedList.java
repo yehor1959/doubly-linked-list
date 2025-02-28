74d8c37c0f1fca537be82690c0a07990e876e74c
yehor1959 <zaitsev.yehor@gmail.com>
2025-02-28 08:55:37 +0100

    Add RemoveLast method to DoublyLinkedList

--- src/main/java/org/example/DoublyLinkedList.java
+++ src/main/java/org/example/DoublyLinkedList.java
@@ -55,4 +55,19 @@ public class DoublyLinkedList {
         }
         length++;
     }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
 }

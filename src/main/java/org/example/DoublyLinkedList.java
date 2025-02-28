3ee2e05b7c857356ad90fe9cddb5eb253ee50a88
yehor1959 <zaitsev.yehor@gmail.com>
2025-02-28 10:10:54 +0100

    Add Get method to DoublyLinkedList

--- src/main/java/org/example/DoublyLinkedList.java
+++ src/main/java/org/example/DoublyLinkedList.java
@@ -98,4 +98,20 @@ public class DoublyLinkedList {
         length--;
         return temp;
     }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        if (index < length/2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
 }
